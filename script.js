067ccd0442872d0dbb7f60a296ab2190ace819ea
adhithiyan <adhithiyan.work@gmail.com>
2023-08-12 14:44:06 +0530

    add validation for password input

--- script.js
+++ script.js
@@ -1,4 +1,14 @@
+/* adding invalid background coloring for required input after user clicks on the field */
 let inp = document.querySelectorAll("input")
 inp.forEach(n => n.addEventListener("click", function(){
     n.classList.add("inputInvalid")
 }))
+
+/* to check if password and confirm password has the same characters */
+let pass = document.querySelector("#pass")
+let confirmPass = document.querySelector("#confirm-pass")
+confirmPass.addEventListener("click",function(){
+    if(pass.value != confirmPass.value){
+        confirmPass.setAttribute("pattern" , `${pass.value}`);
+    }
+})
